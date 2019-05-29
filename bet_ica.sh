f1d6ebb06af02fb3f24b5cd4b15be8ade00aff7f
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-05-29 16:56:46 -0400

    Update bet_ica.sh

--- bet_ica.sh
+++ bet_ica.sh
@@ -4,7 +4,6 @@ module load fsl/5.0.11
 
 filepath=<filepath to data>
 T1MPRAGE=<T1-weighted structural nifti path and file name>
-rsBOLD=<resting-state bold nifti path and name>
 
 for i in <subjects>
 do
@@ -12,11 +11,9 @@ do
 #creates directories in which to move data that is needed for DTI preprocessing scripts
 	mkdir $i
 	mkdir $i/T1
-	mkdir $i/rsBOLD
 
 #copies needed data from shared individual folder to working directory
 	cp $filepath/$i/$T1MPRAGE $i/T1/T1.nii
-	cp $filepath/$i/$rsbold $i/rsBOLD/rsBOLD.nii
 
 #copies slurm script into subject folder
 	cp bet_ica_run.sh $i/
@@ -26,7 +23,7 @@ do
 
 #drops down into subject folder, pushes job to slurm, and returns to home folder
 	cd $i/
-	sbatch bet_ica_run.sh
+	sbatch bet_run.sh
 	cd ../
 
 echo "completed for" $i
